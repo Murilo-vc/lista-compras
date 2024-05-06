7ddb2a5e369a2337341043d03ac93be881aebaf6
Igor <igoreduardo20101@gmail.com>
2024-05-06 20:04:59 -0300

    Valida usuario usando classe Usuario

--- script/entities/usuario.js
+++ script/entities/usuario.js
@@ -1,9 +1,22 @@
 export class Usuario {
-
     constructor(username, senha){
-
         this.username = username;
         this.senha = senha;
         this.listas = [];
+    }
+
+    static getByUsername(username){
+        for (let index = 0; index < localStorage.length; index++) {
+            var id = localStorage.key(index);
+            var usuario = JSON.parse(localStorage.getItem(id));
+            if(usuario.username === username){
+                return usuario;
+            }
+        }
+    }
+
+    //funcao pra uso futuro para adicionar os ids das listas ao array
+    adicionarLista(id){
+        this.listas.push(id);
     }
 }
